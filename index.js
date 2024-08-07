8b79c96fe97d66682a6a1d9c4b2ce597aa5a4b97
LuckyClover <rlathdgus1@gmail.com>
2024-08-07 22:58:52 +0900

    베타 커밋

--- index.js
+++ index.js
@@ -162,6 +162,8 @@ client.on("messageCreate", async (message) => {
 const job = schedule.scheduleJob('0 * * * * *', async function () {
     const notiJson = JSON.parse(fs.readFileSync("./notification.json", "utf8"));
 
+    console.log(notiJson);
+
     notiJson.map(async (item,i) => {
         const channel = client.channels.fetch(item.channel);
 
